792e3c7bc9b922947824c3f9610e1d0f8a88228e
Alvaro Padilla <monster_what@hotmail.com>
2022-09-17 13:05:18 -0600

    Included Kotlin Multi-platform App
    Included the generated template for kotlin multi-platform app

---
+++ ElPatitoNegro/shared/src/commonMain/kotlin/com/thelaunchpad/Greeting.kt
@@ -0,0 +1,9 @@
+package com.thelaunchpad
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greeting(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ ElPatitoNegro/shared/src/commonMain/kotlin/com/thelaunchpad/Platform.kt
@@ -0,0 +1,7 @@
+package com.thelaunchpad
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
