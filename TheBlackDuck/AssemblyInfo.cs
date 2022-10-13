4164677a55db6fdaffc96121aea5cb4bb10009c9
Alvaro Padilla <monster_what@hotmail.com>
2022-10-12 20:18:24 -0600

    Update MVVM

--- TheBlackDuck/AssemblyInfo.cs
+++ TheBlackDuck/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("SCHOOL D3.ttf", Alias = "School") ]
---
+++ TheBlackDuck/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheBlackDuck.ViewModels
+{
+    internal class LoginPageViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginPageViewModel()
+        {
+            Username = "admin";
+            Password = "admin";
+        }
+        
+            
+
+    }
+}
