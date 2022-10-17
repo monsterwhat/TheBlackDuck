bfe30434529a3efafb3181a043abf8c854552ca3
Theal <Theal@ALE_PC>
2022-10-17 17:56:47 -0600

    Update a uso vmcs

---
+++ SyncBlackDuck/SyncBlackDuck/ViewModel/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Sync_test;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SyncBlackDuck.ViewModel
+{
+    public class LoginViewModel 
+    {
+        public Command LoginCommand;
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => 
+            await LoginAsync());
+        }
+        Task LoginAsync()
+        {
+            NavigationPage np = new NavigationPage(new MainPage());
+            Application.Current.MainPage = np;
+            return Task.CompletedTask;
+        }
+    }
+}
--- SyncBlackDuck/SyncBlackDuck/Views/MainPage.xaml.cs
+++ SyncBlackDuck/SyncBlackDuck/Views/MainPage.xaml.cs
@@ -1,114 +1,22 @@
-﻿using Syncfusion.XForms.TextInputLayout;
-using Syncfusion.XForms.Buttons;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-using TheBlackDuck.Models.Implementations;
-using SyncBlackDuck.Services;
+﻿using Xamarin.Forms;
+using SyncBlackDuck.ViewModel;
 
 namespace Sync_test
 {
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
-            InitializeComponent();
-            LoadData();
+            this.InitializeComponent();
+            this.LoginViewModel = new LoginViewModel();
+            this.BindingContext = LoginViewModel;
         }
+        public LoginViewModel LoginViewModel { get; set; }
 
-
-        private void LoadData()
+        /*private void IngresoBtn_Clicked(object sender, EventArgs e)
         {
-
-        }
-
-        public int VerifyLogin()
-        {
-            try
-            {
-
-                int telefono = Convert.ToInt32(txt_telefono.Text);
-                string password = txt_password.Text;
-
-                if (!telefono.Equals(null))
-                {
-                    if (!password.Equals(null))
-                    {
-                        switch (Login(telefono, password))
-                        {
-                            case "cliente":
-                                return 1;
-                            case "admin":
-                                return 2;
-                            case "superadmin":
-                                return 3;
-                            default:
-                                DisplayAlert("Error", "La contraseña es incorrecta", "OK");
-                                return 0;
-                        }
-                    }
-                }
-                return 0;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return 0;
-            }
-        }
-
-        public String Login(int t, string p)
-        {
-            try
-            {
-                loginService login = new loginService();
-                string rank = login.loginByRank(t, p);
-                if (rank != null)
-                {
-                    return rank;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return null;
-
-            }
-        }
-
-        private void IngresoBtn_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                int rank = VerifyLogin();
-                switch (rank)
-                {
-                    case 1:
-                        //Navigation.PushModalAsync(new ClientePage());
-                        break;
-                    case 2:
-                        Navigation.PushModalAsync(new AdminMainPage());
-                        break;
-                    case 3:
-                        //Navigation.PushModalAsync(new SuperAdminPage());
-                        break;
-                    default:
-                        DisplayAlert("Error", "El usuario no existe", "OK");
-                        break;
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception);
-            }
-        }
+            Navigation.PushModalAsync(new AdminMainPage());
+        }*/
     }
 }
