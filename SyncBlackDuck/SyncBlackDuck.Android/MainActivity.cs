b10f678f448ceb214044339f81bb4cb8317aa9a4
Alvaro Padilla <monster_what@hotmail.com>
2022-10-21 20:37:05 -0600

    Update sin navbar

--- SyncBlackDuck/SyncBlackDuck.Android/MainActivity.cs
+++ SyncBlackDuck/SyncBlackDuck.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Views;
 
 namespace SyncBlackDuck.Droid
 {
@@ -13,6 +14,7 @@ namespace SyncBlackDuck.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
